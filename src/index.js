39390a5e07db49bd9ecb84fed329b4c5dcb83cef
homayoun yadegari <homymetropol@gmail.com>
2023-05-27 11:35:48 +0300

    route eklendi

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router,  Route,Routes } from "react-router-dom";
 import './index.css';
 import App from './App';
 import App2 from './app2';
@@ -9,10 +10,13 @@ import DrogDrop from './DrogDrop';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App/> */}
-       <App2 />    
-       {/* <SignIn />    */}
-       {/* <DrogDrop/>    */}
+    <Router>
+    <Routes>
+    <Route path='/' element={<App2/>} />
+    <Route path='/signin' element={<SignIn/>} />
+    <Route path='/drogdrop' element={<DrogDrop/>} />
+      </Routes>
+       </Router>
   </React.StrictMode>
 );
 
